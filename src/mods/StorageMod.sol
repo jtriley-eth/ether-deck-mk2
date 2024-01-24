54b4da7e438dd135438b04856b1bf56485ff6949
jtriley.eth <Jtriley15@gmail.com>
2024-01-24 16:38:41 -0600

    add batch sload

--- src/mods/StorageMod.sol
+++ src/mods/StorageMod.sol
@@ -43,4 +43,42 @@ contract StorageMod {
             }
         }
     }
+
+    /// @notice reads storage in batch
+    /// @dev Directives:
+    ///      01. load key offset; cache as keyOffset
+    ///      02. load array offset; cache as arrayOffset
+    ///      03. store slots length in memory
+    ///      04. loop:
+    ///          a. load key from calldata
+    ///          b. if key is zero, break loop
+    ///          c. load value from storage; store in memory
+    ///          d. increment key offset
+    ///          e. increment array offset
+    ///      05. return array
+    /// @param slots the slots to read
+    /// @return array the values read
+    function read(bytes32[] calldata slots) external view returns (bytes32[] memory) {
+        assembly {
+            let keyOffset := slots.offset
+
+            let arrayOffset := 0x20
+
+            mstore(0x00, slots.length)
+
+            for { } 1 { } {
+                let key := calldataload(keyOffset)
+
+                if iszero(key) { break }
+
+                mstore(arrayOffset, sload(key))
+
+                keyOffset := add(keyOffset, 0x20)
+
+                arrayOffset := add(arrayOffset, 0x20)
+            }
+
+            return(0x00, add(0x20, shr(0x05, slots.length)))
+        }
+    }
 }
